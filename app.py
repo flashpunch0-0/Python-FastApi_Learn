760c6383e1ed1e05dce2e9a7fa1f3f35fded5c33
KRATIK_BOHRA <kratikbohra5@gmail.com>
2023-09-16 02:17:15 +0530

    learn basics of FastApi

---
+++ app.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, Path
+app = FastAPI()
+
+inventory = {
+    1: {
+        "name" : "Milk",
+        "Price" : 35,
+        "Brand" : "amul"
+    }
+}
+
+@app.get("/get-item/{item_id}")
+def get_item(item_id: int = Path( description = "the id of the item you want to get from inventory")):
+    return inventory[item_id]
+
+@app.get("/get-by-name")
+def get_item(name:str):
+    for item_id in inventory:
+        if inventory[item_id]["name"] == name:
+            return inventory[item_id]
+    return {"DAta" : "Not found"}
