1359429b3cd1bebce3ed375089e57719c0a64fb2
KRATIK_BOHRA <kratikbohra5@gmail.com>
2023-09-16 03:12:21 +0530

    basic of post method

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI, Path
+from typing import Optional
+from pydantic import BaseModel
 app = FastAPI()
-
+class Item(BaseModel):
+    name :str
+    price:float
+    brand: Optional[str] = None
+    
 inventory = {
     1: {
         "name" : "Milk",
@@ -19,3 +25,7 @@ def get_item(name:str):
         if inventory[item_id]["name"] == name:
             return inventory[item_id]
     return {"DAta" : "Not found"}
+
+@app.post("/create-item")
+def create_item(item: Item):
+    return {}
