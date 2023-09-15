1afa846331ddba8f26a1ea5762d0f90a951d3fe5
KRATIK_BOHRA <kratikbohra5@gmail.com>
2023-09-16 03:40:34 +0530

    Learnt Post method and its techniques

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from fastapi import FastAPI, Path
 from typing import Optional
+from pydantic import BaseModel
 app = FastAPI()
-
-inventory = {
-    1: {
-        "name" : "Milk",
-        "Price" : 35,
-        "Brand" : "amul"
-    }
-}
+class Item(BaseModel):
+    name :str
+    price:float
+    brand: Optional[str] = None
+inventory = {}
+# inventory = {
+#     1: {
+#         "name" : "Milk",
+#         "Price" : 35,
+#         "Brand" : "amul"
+#     }
+# }
 
 @app.get("/get-item/{item_id}")
 def get_item(item_id: int = Path( description = "the id of the item you want to get from inventory")):
@@ -17,6 +22,14 @@ def get_item(item_id: int = Path( description = "the id of the item you want to 
 @app.get("/get-by-name/{item_id}")
 def get_item(*,item_id :int,name: Optional[str] = None, test: int):
     for item_id in inventory:
-        if inventory[item_id]["name"] == name:
+        if inventory[item_id].name == name:
             return inventory[item_id]
     return {"DAta" : "Not found"}
+
+@app.post("/create-item/{item_id}")
+def create_item(item_id : int, item: Item):
+    if item_id in inventory:
+        return {"Error ": "Item Id already exists"}
+    # inventory[item_id] = {"name" : item.name , "brand":item.brand,  "price": item.price}
+    inventory[item_id] = item
+    return  inventory[item_id] 
