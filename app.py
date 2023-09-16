e1178353e99d59caf81ca98808fcf2d46d03e021
KRATIK_BOHRA <kratikbohra5@gmail.com>
2023-09-16 15:21:32 +0530

    learned update method in Fastapi

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ class Item(BaseModel):
     name :str
     price:float
     brand: Optional[str] = None
+class UpdateItem(BaseModel):
+    name :Optional[str] = None
+    price:Optional[float] = None
+    brand: Optional[str] = None
+    
+
 inventory = {}
 # inventory = {
 #     1: {
@@ -33,3 +39,18 @@ def create_item(item_id : int, item: Item):
     # inventory[item_id] = {"name" : item.name , "brand":item.brand,  "price": item.price}
     inventory[item_id] = item
     return  inventory[item_id] 
+
+@app.put('/update-item/{item_id}')
+def update_item(item_id : int , item:UpdateItem):
+    if item_id not in inventory:
+        return {"Error" : "Item not in inventory"}
+    if item.name!= None:
+        inventory[item_id].name = item.name
+    if item.price!= None:
+        inventory[item_id].price = item.price
+    if item.brand!= None:
+        inventory[item_id].brand = item.brand
+    # inventory[item_id] = item
+    return inventory[item_id]
+# we have to manually update the items as the it is an instance of BaseModel and not an already made dictionary
+# so now while updating if we only send one value then it will update only that element value and will not require you to specify all other element values
